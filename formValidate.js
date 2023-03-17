4bbc171a93338785399ef936238957bafce9d3a0
Larry Villegas <larry_villegas@hotmail.com>
2023-03-17 17:33:34 -0400

    Update formValidate.js

--- formValidate.js
+++ formValidate.js
@@ -1,29 +1,23 @@
 const myForm = document.getElementById('input-form');
 
-function isNotLower(arg){
-  if(arg.match(/^[a-z@.0-9-_]*$/)){
+function isNotLower(arg) {
+  if (arg.match(/^[a-z@.0-9-_]*$/)) {
     return false;
-  } else {
-    return true;
   }
-};
+  return true;
+}
 
 myForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const msgError = document.getElementById('error');
 
-  if(isNotLower(myForm.elements['email'].value)){
-    let myMessage= `You must write the email in lowercase eg.(mymail@mydomain.ref)`;
+  if (isNotLower(myForm.elements.email.value)) {
+    const myMessage = 'You must write the email in lowercase eg.(mymail@mydomain.ref)';
     msgError.innerHTML = myMessage;
     msgError.classList.add('error-show');
-    
   } else {
-    
-    msgError.innerHTML="";
+    msgError.innerHTML = '';
     msgError.classList.remove('error-show');
     myForm.submit();
   }
-
-
 });
-
