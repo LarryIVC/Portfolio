d33cc9a222ff63e42d35a29395e9dc4ab3e5ddab
Larry Villegas <larry_villegas@hotmail.com>
2023-03-17 01:57:26 -0400

    Refactoring works dinamically

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ const works = [
   {
     name: 'Tonic',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    smalldesc:'Exploring the future of media in Facebook`\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     image: 'image/works1-big.png',
     technologies: ['html', 'css', 'JavaScript'],
     linkLive: 'https://larryivc.github.io/portfolio/',
@@ -10,6 +11,7 @@ const works = [
   {
     name: 'Multi-Post Stories',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    smalldesc:'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     image: 'image/works2.png',
     technologies: ['html', 'Ruby on rails', 'css', 'JavaScript'],
     linkLive: 'https://larryivc.github.io/portfolio/',
@@ -18,14 +20,16 @@ const works = [
   {
     name: 'Facebook 360',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    smalldesc: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'image/works3.png',
-    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript'],
+    technologies: ['html', 'css', 'JavaScript', 'Ruby on rails'],
     linkLive: 'https://larryivc.github.io/portfolio/',
     linkCode: 'https://github.com/LarryIVC/portfolio',
   },
   {
     name: 'Uber Navigation',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.',
+    smalldesc: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     image: 'image/works4.png',
     technologies: ['html', 'Ruby on rails', 'css', 'JavaScript'],
     linkLive: 'https://larryivc.github.io/portfolio/',
@@ -33,13 +37,48 @@ const works = [
 
   },
 ];
+// cards
+const worksContainer = document.querySelector('.works');
+let cardWorks =``;
+works.forEach((datos, index) => {
+  cardWorks  += `<div class="works1 `;  
+  if(index % 2 !== 0){
+    cardWorks += `reverse`;
+  }
+  cardWorks += `" id="portfolio">
+<div class="div-img">
+<img class="img-works" src="${datos.image}" alt="Avocode Snapshoot">                    
+</div>
+<div class="work-desc">
+    <h2 class="title-works">${datos.name}</h2>
+    <div class="sub-box">
+        <h3>CANOPY</h3>
+        <ul>
+            <li>Back End Dev</li>
+            <li>2015</li>
+        </ul>                        
+    </div>
+    <p class="des-p">${datos.smalldesc}</p>
+    <ul class="small-button">`;
+
+    for(let label=0;label<datos.technologies.length; label +=1){
+      cardWorks += `<li>${datos.technologies[label]}</li>`;
+    }
+    cardWorks += `</ul>
+    <button type="button" class="see-button btn-work">See Project</button>
+</div>
+</div>`;
+});
+
+worksContainer.innerHTML= cardWorks;
+// end cards
 
 const openPopup = document.querySelectorAll('.btn-work');
 const popupContainer = document.querySelector('.popup-principal');
 
 for (let pointer = 0; pointer < works.length; pointer += 1) {
   openPopup[pointer].addEventListener('click', () => {
-    const htmlContent = `<div class="popup-details">
+    let htmlContent = `<div class="popup-details">
           <div class="popup-image">
             <img id="popup-close" src="image/btn-x1.svg" alt="Close button">
             <h2 class="title-works">${works[pointer].name}</h2>
@@ -57,10 +96,14 @@ for (let pointer = 0; pointer < works.length; pointer += 1) {
               <p class="popup-txt">${works[pointer].description}</p>
             </div>
             <div class="popup-footer">
-              <ul class="small-button">
-                <li>${works[pointer].technologies[0]}</li>
-                <li>${works[pointer].technologies[1]}</li>
-                <li>${works[pointer].technologies[2]}</li>                              
+              <ul class="small-button">`;
+              
+
+    for(let popupLabel=0; popupLabel < works[pointer].technologies.length; popupLabel += 1) {
+      htmlContent += `<li>${works[pointer].technologies[popupLabel]}</li>`;
+    }
+
+    htmlContent += `                                            
             </ul>
             <hr class="popup-line">
             <div class="action">              
@@ -70,6 +113,7 @@ for (let pointer = 0; pointer < works.length; pointer += 1) {
             </div>
           </div>
         </div>`;
+        
     popupContainer.innerHTML = htmlContent;
 
     const closePopup = document.getElementById('popup-close');
