9c427d4e390eb513f6cbb02be136a20f28173315
Larry Villegas <larry_villegas@hotmail.com>
2023-03-17 17:18:45 -0400

    Add code to formValidate.js

---
+++ formValidate.js
@@ -0,0 +1,31 @@
+const myForm = document.getElementById('input-form');
+
+function isNotLower(arg){
+  if(arg.match(/^[a-z@.0-9-_]*$/)){
+    return false;
+  } else {
+    return true;
+  }
+};
+
+myForm.addEventListener('submit', (event) => {
+  // alert(myForm.elements['email'].value);
+  event.preventDefault();
+  const msgError = document.getElementById('error');
+
+  if(isNotLower(myForm.elements['email'].value)){
+    let myMessage= `You must write the email in lowercase eg.(mymail@mydomain.ref)`;
+    msgError.innerHTML = myMessage;
+    // msgError.innerHTML = myMessage;
+    msgError.classList.add('error-show');
+    
+  } else {
+    
+    msgError.innerHTML="";
+    msgError.classList.remove('error-show');
+    myForm.submit();
+  }
+
+
+});
+
