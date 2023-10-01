63d488a082f6462c97502307ee221aefee06749e
Larry Ivan Villegas Costas <larry_villegas@hotmail.com>
2023-10-01 00:19:09 -0400

    Add properties to cards and svg

--- js/popup.js
+++ js/popup.js
@@ -1,42 +1,5 @@
-const works = [
-  {
-    name: 'Pennywise',
-    description: 'Pennywise: Manage your finances with ease. Track spending, set budgets, gain insights, and make informed decisions for financial freedom. Start today!',
-    smalldesc: 'Manage your finances with ease, spendigs and budgets',
-    image: 'image/pennywise.webp',
-    technologies: ['erb', 'CSS', 'JavaScript', 'Ruby', 'Ruby on rails', 'PostgreSQL'],
-    linkLive: 'https://rails-lb6q.onrender.com/',
-    linkCode: 'https://github.com/LarryIVC/pennywise',
-  },
-  {
-    name: 'Fast Land Racers',
-    description: 'Fast-Lane-Racer is a car booking website that allows users to easily book a ride and travel to their desired destinations. User can reserve a car from the available cars by providing the date and city. Fast-Lane-Racer provides a seamless and reliable car booking experience.',
-    smalldesc: 'Is a car booking website that allows users to easily book a ride and travel to their desired destinations.',
-    image: 'image/mock-racer.webp',
-    technologies: ['html', 'Ruby on rails', 'css', 'JavaScript', 'PostgreSQL', 'React', 'Redux'],
-    linkLive: 'https://shiny-longma-bba6fb.netlify.app',
-    linkCode: 'https://github.com/LarryIVC/Fast-Lane-Racers-front-end',
-  },
-  {
-    name: 'Crypto Coinranking',
-    description: 'Is a powerful web application designed specifically for crypto coins. It leverages the capabilities of React and Redux to provide comprehensive information about various cryptocurrencies.',
-    smalldesc: 'Is a powerful web application designed specifically for crypto coins.',
-    image: 'image/cryptomock.png',
-    technologies: ['html', 'css', 'JavaScript', 'React', 'Redux'],
-    linkLive: 'https://react-coin-coinranking.netlify.app/',
-    linkCode: 'https://github.com/LarryIVC/react-coins',
-  },
-  {
-    name: 'The Simpsons Fans Club',
-    description: 'This project demonstrates my ability to use JavaScript, Webpack, and Jest, as well as reading and writing APIs using JSON.',
-    smalldesc: 'This project demonstrates my ability to use JavaScript, Webpack, and Jest, as well as reading and writing APIs using JSON.',
-    image: 'image/simpmock.jpeg',
-    technologies: ['html', 'css', 'JavaScript'],
-    linkLive: 'https://js-capstoneproject.netlify.app/',
-    linkCode: 'https://github.com/LarryIVC/portfolio',
-  },
-];
-// cards
+import works from './projects.js';
+
 const worksContainer = document.querySelector('.works');
 let cardWorks = '';
 works.forEach((datos, index) => {
@@ -45,28 +8,28 @@ works.forEach((datos, index) => {
     cardWorks += 'reverse';
   }
   cardWorks += `" id="portfolio">
-<div class="div-img">
-<img class="img-works" src="${datos.image}" alt="Avocode Snapshoot">                    
-</div>
-<div class="work-desc">
-    <h2 class="title-works">${datos.name}</h2>
-    <div class="sub-box">
-        <h3>CANOPY</h3>
-        <ul>
-            <li>Back End Dev</li>
-            <li>2023</li>
-        </ul>                        
+    <div class="div-img">
+    <img class="img-works" src="${datos.image}" alt="Avocode Snapshoot">                    
     </div>
-    <p class="des-p">${datos.smalldesc}</p>
-    <ul class="small-button">`;
+    <div class="work-desc">
+        <h2 class="title-works">${datos.name}</h2>
+        <div class="sub-box">
+            <h3>CANOPY</h3>
+            <ul>
+                <li>${datos.canopy}</li>
+                <li>${datos.year}</li>
+            </ul>                        
+        </div>
+        <p class="des-p">${datos.smalldesc}</p>
+  <ul class="small-button">`;
 
   for (let label = 0; label < datos.technologies.length; label += 1) {
     cardWorks += `<li>${datos.technologies[label]}</li>`;
   }
   cardWorks += `</ul>
-    <button type="button" class="see-button btn-work">See Project</button>
-</div>
-</div>`;
+        <button type="button" class="see-button btn-work">See Project</button>
+    </div>
+    </div>`;
 });
 
 worksContainer.innerHTML = cardWorks;
